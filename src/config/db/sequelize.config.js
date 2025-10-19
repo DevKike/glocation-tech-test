ff010fba77c7b8d853757736a25e32ab921448cf
DevKike <alvaronarvaez.dev@gmail.com>
2025-10-19 16:13:37 -0500

    feat: initialize project with Sequelize and PostgreSQL configuration

---
+++ src/config/db/sequelize.config.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+const setupModel = require('../../db/setup-model');
+const environment = require('../environment/environment.local');
+
+const sequelize = new Sequelize(
+  environment.DB_NAME,
+  environment.DB_USER,
+  environment.DB_PASSWORD,
+  {
+    host: environment.DB_HOST,
+    port: environment.DB_PORT,
+    dialect: environment.DB_DIALECT,
+    logging: false,
+  }
+);
+
+setupModel(sequelize);
+
+module.exports = sequelize;
