8941a27ec6212dec5d0f09b174a73fdac36ecfbc
DevKike <alvaronarvaez.dev@gmail.com>
2025-10-19 16:13:18 -0500

    feat: add environment configuration files for database and server settings

---
+++ src/config/environment/environment.local.js
@@ -0,0 +1,15 @@
+const { config } = require('dotenv');
+
+config();
+
+const environment = {
+  SERVER_PORT: process.env.SERVER_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_DIALECT: process.env.DB_DIALECT,
+  DB_PORT: process.env.DB_PORT,
+};
+
+module.exports = environment;
