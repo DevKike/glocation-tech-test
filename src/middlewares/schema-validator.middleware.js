b48ef1da8f44d335e11bd92fefa831b97410b857
DevKike <alvaronarvaez.dev@gmail.com>
2025-10-19 17:31:51 -0500

    refactor: enhance schemaValidator middleware to support validation from query parameters

--- src/middlewares/schema-validator.middleware.js
+++ src/middlewares/schema-validator.middleware.js
@@ -1,6 +1,8 @@
-const schemaValidator = (schema) => {
+const schemaValidator = (schema, source = 'body') => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body);
+    const dataToValidate = source === 'query' ? req.query : req.body;
+
+    const { error } = schema.validate(dataToValidate);
 
     if (error) {
       res.status(400).json({
